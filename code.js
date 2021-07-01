cf548766d6af28d9afe5e01bbc91a0329fee1078
Rock-ass <72495466+Rock-ass@users.noreply.github.com>
2021-07-01 14:51:16 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,18 @@
+onEvent("button1", "click", function( ) {
+  setScreen("screen2");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button15", "click", function( ) {
+  open("https://studio.code.org/projects/gamelab/7tWuXSwIT1AbWWrUfslMcSEq7IhC5qHE3vrNj4QYb3g");
+});
+onEvent("button16", "click", function( ) {
+  open("https://studio.code.org/projects/gamelab/6M4igsqAQbFlX4m8BT1h-IqeIzViJ2uYDa8IcF73DHw");
+});
+onEvent("button18", "click", function( ) {
+  open("https://studio.code.org/projects/gamelab/KXD-y1wt52gkUPh4N9Cc1A9gJxirZUEu_6J9BJYeIk8");
+});
+onEvent("button17", "click", function( ) {
+  open("https://studio.code.org/projects/gamelab/vcK_GY7y9AnUIzBK122VkbhDJd7ZY04bL674IV_LEzI");
+});
